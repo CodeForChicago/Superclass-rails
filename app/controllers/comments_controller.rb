86f019fd2e4c5526c054aaed255d7522fff580ec
Juan David <juandominguez2017@u.northwestern.edu>
2015-08-13 21:20:43 -0700

    Adds create comment functionality to API

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,19 @@
 class CommentsController < ApplicationController
-	
+
+	#POST /comments
+	def create
+		comment = Comment.new(comments_params)
+		if comment.save
+			render json: comment, serializer: CommentsSerializer
+		end
+		# else
+		# 	render json: {error: ('comment_create_error')}, status: :unprocessable_entity
+		# end
+	end
+
+	def comments_params
+		comment = params.require(:comment).permit(:body, :question)
+		{user: current_user, question: Question.find(comment["question"]), body: comment["body"]}
+	end
+
 end
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -4,15 +4,20 @@ require 'spec_helper'
 describe CommentsController do
 	describe 'POST' do
 		it 'adds a comment to the database' do
+			test_authorize_session
+
 			question = create(:question)
 			user = create(:user)
 			expect {
 				post :create, comment: {
 					body: "Hello, I am a comment",
-					question: question.id,
-					user: user.id
+					question: question.id
 				}
 			}.to change(Comment, :count).by 1
 		end
+
+		it 'returns an error if comment did not save due to RunTimeError'
+
+		it 'returns a 401 if the user is not authorized'
 	end
 end
