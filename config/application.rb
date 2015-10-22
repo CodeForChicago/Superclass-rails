51b9d9fd3039aa4df9689290186236dbddfc2458
MichaelJH <michaelhorst2016@u.northwestern.edu>
2015-10-21 20:23:31 -0500

    Made the Feedback form send e-mails to the CfC account.

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => "587",
   :domain               => 'gmail.com',
-  :user_name            => 'myaccount@mymail.com',
-  :password             => 'mypassword',
+  :user_name            => 'CodeforChicago@u.northwestern.edu',
+  :password             => 'mhy4&HWS',
   :authentication       => 'plain',
   :enable_starttls_auto => true  }
 
