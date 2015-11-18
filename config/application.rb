03f557d7bb1f5b0de064f27a542f4248ea5160ea
Juan David <juandominguez2017@u.northwestern.edu>
2015-11-18 17:09:51 -0600

    Adding support for local variables

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,15 @@
 require File.expand_path('../boot', __FILE__)
-
 require 'rails/all'
+
+
+config.before_configuration do
+  env_file = File.join(Rails.root, 'config', 'local_env.yml')
+  YAML.load(File.open(env_file)).each do |key, value|
+    ENV[key.to_s] = value
+  end if File.exists?(env_file)
+end
+
+
 
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.perform_deliveries = true
