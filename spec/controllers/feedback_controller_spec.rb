d387908f63831a1466b1058016d66d9a758a5c31
Juan David <juandominguez2017@u.northwestern.edu>
2015-12-05 13:57:41 -0600

    Begins new feedback possibilities

--- spec/controllers/feedback_controller_spec.rb
+++ spec/controllers/feedback_controller_spec.rb
@@ -2,11 +2,19 @@ require "spec_helper"
 
 describe FeedbackController do
   describe 'POST create' do
-    it 'sends an email' do
+    it 'sends an email from cfc main page' do
       test_authorize_session
       expect {post :create, email: {
         body: Faker::Lorem.paragraph
       }}.to change { ActionMailer::Base.deliveries.count  }.by(1)
+    end
+
+    it 'sends an email from cfc club page' do
+    	expect {post :create, email: {
+    		body: Faker::Lorem.paragraph,
+    		username: Faker::Name.name,
+    		useremail: Faker::Internet.email
+    		}}.to change { ActionMailer::Base.deliveries.count }.by(1)
     end
   end
 end
