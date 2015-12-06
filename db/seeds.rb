d6db2a24f781411151b68443f91e1b0bf4610389
Juan David <juandominguez2017@u.northwestern.edu>
2015-12-06 12:05:40 -0600

    Corrects the db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-if Rails.env.test?
+if Rails.env.production?
+	User.create(email: "codeforchicago@u.northwestern.edu", username: 'CFC', password: ENV['ADMIN_PW'], role: 'admin')
+else
 	require 'faker'
 
 	10.times do
-	  Lesson.create(summary: Faker::Lorem.paragraph, title: Faker::Name.title, creator: "CFC", link: "codeacademy.com")
+	  Lesson.create(summary: Faker::Lorem.paragraph, title: Faker::Name.title, creator: "CFC", link: "http://www.codeacademy.com")
 	  u = User.create(email: Faker::Internet.email, username: Faker::Internet.user_name, password: 'password')
 	  q = Question.create(user: u, body: Faker::Lorem.paragraph, title: Faker::Name.title)
 
@@ -18,7 +20,5 @@ if Rails.env.test?
 	end
 	User.create(email: "user@user.com", username: 'user', password: 'password')
 	User.create(email: "codeforchicago@u.northwestern.edu", username: 'CFC', password: 'password', role: 'admin')
-else
-	User.create(email: "codeforchicago@u.northwestern.edu", username: 'CFC', password: ENV['ADMIN_PW'], role: 'admin')
 end
 
